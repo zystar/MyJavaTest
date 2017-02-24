7293479fd872057fe7de426f5af2f35bf4d136b2
zystar <1803999267@qq.com>
2017-02-24 22:03:40 +0800

    简单的实现单链表的基本操作

---
+++ src/com/test/nodeTest/TestLinkList.java
@@ -0,0 +1,37 @@
+package com.test.nodeTest;
+
+/**
+ * @author STAR
+ * 用java简单的实现单链表的基本操作
+ * 参考：http://blog.csdn.net/tayanxunhua/article/details/11100097/
+ */
+public class TestLinkList {
+
+	public static void main(String[] args) {
+		LinkList linkList = new LinkList();  
+        linkList.addFirstNode(20);  
+        linkList.addFirstNode(21);  
+        linkList.addFirstNode(19);  
+         //19,21,20  
+        linkList.add(1, 22); //19,22,21,20  
+        linkList.add(2, 23); //19,22,23,21,20  
+        linkList.add(3, 99); //19,22,23,99,21,20  
+        linkList.displayAllNodes();  
+//      Node node = linkList.deleteFirstNode();  
+//      System.out.println("node : " + node.data);  
+//      linkList.displayAllNodes();  
+//      node = linkList.deleteByPos(2);  
+//      System.out.println("node : " + node.data);  
+//      linkList.displayAllNodes();  
+//      linkList.deleteFirstNode();  
+        Node node = linkList.deleteByData(19);  
+//      Node node = linkList.deleteByPos(0);  
+        System. out.println( "node : " + node. data);  
+        linkList.displayAllNodes();  
+        Node node1 = linkList.findByPos(0);  
+        System. out.println( "node1: " + node1. data);  
+        Node node2 = linkList.findByData(22);  
+        System. out.println( "node2: " + node2. data);  
+	}
+
+}
