ccd29f1c42d425b22f2f7569df158af29eeb6333
zystar <1803999267@qq.com>
2017-03-08 22:48:10 +0800

    二叉树后序中序

--- src/com/test/nodeTest/TreeNodeTest.java
+++ src/com/test/nodeTest/TreeNodeTest.java
@@ -1,6 +1,9 @@
 package com.test.nodeTest;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
 import java.util.List;
 import java.util.Stack;
 
@@ -28,6 +31,48 @@ public class TreeNodeTest {
 		}
 		return result;
 	}
+	//后序遍历
+	public List<Integer> postorderTraversal(TreeNode root){
+		List<Integer> result = new ArrayList<>();
+		if(root == null){
+			return result;
+		}
+		Deque<TreeNode> stack = new ArrayDeque<>();
+		stack.push(root);
+		while(!stack.isEmpty()){
+			TreeNode node = stack.pop();
+			result.add(node.val);
+			if(node.left != null)
+				stack.push(node.left);
+			if(node.right != null)
+				stack.push(node.right);
+		}
+		Collections.reverse(result);
+		return result;
+	}
+	/**
+	 * 中序遍历
+	 * @param root
+	 * @中序遍历思路: 中序遍历迭代法思路不同于前序和后序.
+	 * 首先针对对当前节点,一直对其左子树迭代并将非空节点入栈
+     * 节点指针迭代为空(到树底了)后不再入栈,然后取栈顶元素,存结果;
+     * 将当前指针用出栈的节点的右子节点替代,然后回到第一步继续迭代,直到当前节点为空且栈为空.
+	 */
+	public List<Integer> inorderTraversal(TreeNode root){
+	      List<Integer> result = new ArrayList<>();
+	      if(root==null) return result;
+	      Deque<TreeNode> stack = new ArrayDeque<>();
+	      while (root!=null||!stack.isEmpty()){
+	          while(root!=null){
+	              stack.push(root);
+	              root=root.left;
+	          }
+	          TreeNode node=stack.pop();
+	          result.add(node.val);
+	          root=node.right;
+	      }
+	      return result;
+	  }
 }
 class TreeNode{
 	int val;
